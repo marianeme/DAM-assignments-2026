615ede09fa46089a38626d06d8644ffb89d896b1
marianeme <marianedmsilva@gmail.com>
2026-07-06 22:00:32 -0300

    feat: configure app bar theme

--- assignment_01_layout_widgets/lib/main.dart
+++ assignment_01_layout_widgets/lib/main.dart
@@ -11,29 +11,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Pedido Flash',
       theme: ThemeData(
-          colorScheme: .fromSeed(seedColor: Colors.deepOrange),
-      textTheme: TextTheme(
-        headlineSmall: TextStyle(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color(0xFFFF5A1F),
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFFFF5A1F),
+          foregroundColor: Colors.black,
+          elevation: 0,
+          centerTitle: false,
+        ),
+        textTheme: TextTheme(
+          headlineSmall: TextStyle(
           fontSize: 24,
           fontWeight: FontWeight.bold,
-        ),
-        titleLarge: TextStyle(
-          fontSize: 22,
-          fontWeight: FontWeight.w600,
-        ),
-        bodyLarge: TextStyle(
-          fontSize: 16,
-          fontWeight: FontWeight.normal,
-        ),
-        bodyMedium: TextStyle(
-          fontSize: 14,
-        ),
-        labelLarge: TextStyle(
-          fontSize: 14,
-          fontWeight: FontWeight.w600,
+          ),
+          titleLarge: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w600,
+          ),
+          bodyLarge: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.normal,
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 14,
+          ),
+          labelLarge: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+          ),
         ),
       ),
-    ),
       home: Scaffold(
           appBar: AppBar(
             title: Text("Pedido Flash"),
