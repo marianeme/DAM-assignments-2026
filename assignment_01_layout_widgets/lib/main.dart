299fffeb633c5ee0ee6bafdef95bd63ecb518a35
marianeme <marianedmsilva@gmail.com>
2026-07-06 21:00:39 -0300

    feat: configure global application theme

--- assignment_01_layout_widgets/lib/main.dart
+++ assignment_01_layout_widgets/lib/main.dart
@@ -10,6 +10,30 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pedido Flash',
+      theme: ThemeData(
+          colorScheme: .fromSeed(seedColor: Colors.deepOrange),
+      textTheme: TextTheme(
+        headlineSmall: TextStyle(
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
+        ),
+        titleLarge: TextStyle(
+          fontSize: 22,
+          fontWeight: FontWeight.w600,
+        ),
+        bodyLarge: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.normal,
+        ),
+        bodyMedium: TextStyle(
+          fontSize: 14,
+        ),
+        labelLarge: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    ),
       home: Scaffold(
           appBar: AppBar(
             title: Text("Pedido Flash"),
